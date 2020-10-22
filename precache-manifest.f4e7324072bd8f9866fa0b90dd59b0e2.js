acf202ea3e8ac6336d1d4ee5027fd4fab584acc1
Shivank Mathur <shivumat@gmail.com>
2020-10-22 21:00:06 +0530

    Updates

---
+++ precache-manifest.f4e7324072bd8f9866fa0b90dd59b0e2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d280cad0a44f8d2c0301ac45f7cb174",
+    "url": "/Web-Resume/index.html"
+  },
+  {
+    "revision": "b869778b389eb0b39179",
+    "url": "/Web-Resume/static/css/main.20b59997.chunk.css"
+  },
+  {
+    "revision": "20eca35f0685eeb99140",
+    "url": "/Web-Resume/static/js/2.a444154f.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/Web-Resume/static/js/2.a444154f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b869778b389eb0b39179",
+    "url": "/Web-Resume/static/js/main.b188d7d1.chunk.js"
+  },
+  {
+    "revision": "f2bbd1f8860686ae21ef",
+    "url": "/Web-Resume/static/js/runtime-main.34a24f63.js"
+  },
+  {
+    "revision": "cf50c0e61f68eeb8e024c09235b31dc2",
+    "url": "/Web-Resume/static/media/3.cf50c0e6.jpeg"
+  },
+  {
+    "revision": "fcaf0ac09d24105cd78fcd27d05ab763",
+    "url": "/Web-Resume/static/media/Mathur_Shivank.fcaf0ac0.pdf"
+  },
+  {
+    "revision": "ab2074824038e3834203fa8a3210b42c",
+    "url": "/Web-Resume/static/media/RFID_bus.ab207482.jpg"
+  },
+  {
+    "revision": "105c0232ebf29ceb00819765ed7a5a81",
+    "url": "/Web-Resume/static/media/background.105c0232.jpg"
+  },
+  {
+    "revision": "86f228bbf651f9794fba2b4e56363a3b",
+    "url": "/Web-Resume/static/media/logo.86f228bb.png"
+  },
+  {
+    "revision": "16dc51224ab04f2d79c4648a99152338",
+    "url": "/Web-Resume/static/media/smart-fridge.16dc5122.png"
+  },
+  {
+    "revision": "9607ca2dc1cd7c4d641b33eadd06e109",
+    "url": "/Web-Resume/static/media/titleImage.9607ca2d.jpg"
+  }
+]);
